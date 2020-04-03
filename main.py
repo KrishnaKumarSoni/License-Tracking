188379bdb1c3a24118e09005d485041356b0b91c
KrishnaKumarSoni <39898943+KrishnaKumarSoni@users.noreply.github.com>
2020-04-03 21:59:14 +0530

    connected with tesseract

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #this file is used to make the predictions on the model 
 #which was trained in locatingPlate.py
 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import glob
 import os
+import time
+from PIL import Image
+import pytesseract
 
 from keras.models import Model, Sequential, load_model
 
@@ -13,7 +18,7 @@ def locatePlate(filename):
     WIDTH = 224
     HEIGHT = 224
 
-    new_model = load_model('License_Plate_Locator.hd5')
+    new_model = load_model('License Plate Locator.hd5')
     img = cv2.resize(cv2.imread("static/images/"+filename) / 255.0, dsize=(WIDTH, HEIGHT))
     y_hat = new_model.predict(img.reshape(1, WIDTH, HEIGHT, 3)).reshape(-1) * WIDTH
 
@@ -25,3 +30,35 @@ def locatePlate(filename):
     plt.imshow(image)
     plt.axis('off')
     plt.savefig('static/images/o_'+filename, bbox_inches='tight')
+
+    widthnew = 450
+    heightnew = 450
+    originalImage = cv2.resize(cv2.imread("static/images/"+filename), dsize=(widthnew, heightnew))
+    originalImage = cv2.resize(originalImage, (widthnew, heightnew))
+    xtn =  xt*widthnew/224
+    ytn =  yt*widthnew/224
+    xbn =  xb*widthnew/224
+    ybn =  yb*widthnew/224
+    originalImage = cv2.rectangle(originalImage, (int(xtn), int(ytn)), (int(xbn), int(ybn)), (0, 0, 255), 1)
+    
+    #Cropping out the Region of Interest
+    start_row, start_column = int(ytn), int(xtn)
+    end_row, end_column = int(ybn), int(xbn)
+    roi = originalImage[start_row:end_row, start_column:end_column]
+    plt.imshow(roi)
+    plt.axis('off')
+    plt.savefig('static/images/plate_'+filename, bbox_inches='tight')
+
+    #Reading the License Plate
+    plateImg = cv2.imread("static/images/plate_"+filename)
+    gblur = cv2.GaussianBlur(plateImg, (5, 5), 0)
+    _, th = cv2.threshold(gblur, 50, 255, cv2.THRESH_BINARY_INV)
+
+    plt.imshow(th)
+    plt.axis('off')
+    plt.savefig('static/images/forChar_'+filename, bbox_inches='tight')
+ 
+    #this line needs to be changed while running on your pc 
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    textToShow = pytesseract.image_to_string(Image.open('static/images/forChar_'+filename))
+    return textToShow
